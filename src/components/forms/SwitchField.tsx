8f5d41caf343e44746b9df7f0726d7b47fa38c92
SonMooSans <god63820869@gmail.com>
2022-12-09 22:02:39 +0800

    Create Switch field component

---
+++ src/components/forms/SwitchField.tsx
@@ -0,0 +1,27 @@
+// Chakra imports
+import { Flex, FormLabel, Switch, SwitchProps, Text, useColorModeValue } from '@chakra-ui/react';
+// Custom components
+export function SwitchField(
+  props: {
+    id: string;
+    label?: string;
+    desc?: string;
+  } & SwitchProps
+) {
+  const { id, label, desc, ...rest } = props;
+
+  const textColorPrimary = useColorModeValue('secondaryGray.900', 'white');
+  return (
+    <Flex justify="space-between" align="center" borderRadius="16px">
+      <FormLabel htmlFor={id} _hover={{ cursor: 'pointer' }} flexDirection="column">
+        <Text color={textColorPrimary} fontSize="md" fontWeight="600">
+          {label}
+        </Text>
+        <Text color="secondaryGray.600" fontSize="md">
+          {desc}
+        </Text>
+      </FormLabel>
+      <Switch id={id} variant="main" size="md" {...rest} />
+    </Flex>
+  );
+}
