d05f09cb29bf18158a860eed008025b04072a2dc
SonMooSans <god63820869@gmail.com>
2022-12-13 18:10:06 +0800

    Upgrade to vite 3.0

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,31 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import viteTsconfigPaths from 'vite-tsconfig-paths';
 import svgrPlugin from 'vite-plugin-svgr';
+import path from 'path';
 
 export default defineConfig({
-  plugins: [react(), viteTsconfigPaths(), svgrPlugin()],
+  plugins: [
+    react({
+      include: ['**/*.tsx', '**/*.jsx'],
+    }),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+  ],
+  resolve: {
+    alias: {
+      components: path.resolve(__dirname, './src/components'),
+      assets: path.resolve(__dirname, './src/assets'),
+      stores: path.resolve(__dirname, './src/stores'),
+      theme: path.resolve(__dirname, './src/theme'),
+      config: path.resolve(__dirname, './src/config'),
+      api: path.resolve(__dirname, './src/api'),
+      layouts: path.resolve(__dirname, './src/layouts'),
+      sidebar: path.resolve(__dirname, './src/sidebar.tsx'),
+      utils: path.resolve(__dirname, './src/utils'),
+      views: path.resolve(__dirname, './src/views'),
+      hooks: path.resolve(__dirname, './src/hooks'),
+    },
+  },
   server: {
     host: 'localhost',
     port: 3000,
