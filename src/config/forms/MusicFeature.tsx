5dd07782bc652040c0deb328bea31a0208c832b9
SonMooSans <god63820869@gmail.com>
2022-12-14 18:19:24 +0800

    Improve example feature

--- src/config/forms/MusicFeature.tsx
+++ src/config/forms/MusicFeature.tsx
@@ -2,12 +2,14 @@ import { SimpleGrid } from '@chakra-ui/layout';
 import { MusicFeature } from 'config/types';
 import { ChannelSelect } from '../example/ChannelSelect';
 import { RolesSelect } from '../example/RolesSelect';
-import { createForm } from 'hooks/forms/createForm';
-import { useForm } from 'hooks/forms/useForm';
+import { useFormRender } from 'hooks/forms/useForm';
+import { useMemo } from 'react';
 
 export function useMusicFeature(data: MusicFeature) {
-  const { value, update, errors, ...form } = useForm<Partial<MusicFeature>>({
-    defaultValue: { count: '2' },
+  const base = useMemo(() => ({ ...data, count: '2' }), [data]);
+
+  return useFormRender<Partial<MusicFeature>>({
+    defaultValue: base,
     verify: (v, errors) => {
       if (v.message != null && v.message.trim().length === 0) {
         errors.message = "Message can't be emtpy or blank";
@@ -15,99 +17,96 @@ export function useMusicFeature(data: MusicFeature) {
 
       if (v.count === '0') errors.count = "Can't be 0";
     },
+    container: (f) => (
+      <SimpleGrid columns={{ base: 1, md: 2 }} gap={3}>
+        {f}
+      </SimpleGrid>
+    ),
+    render: ({ value, update, errors, ...form }) => [
+      {
+        defaultMemorize: ['value', 'error'],
+      },
+      {
+        label: 'Message',
+        description: 'Hello World!!!',
+        type: 'input',
+        value: value.message,
+        onChange: (message) => update({ message }),
+        error: errors.message,
+      },
+      {
+        type: 'input',
+        label: 'Count',
+        placeholder: 'Put a number',
+        value: value.count ?? '0',
+        onChange: (v) => update({ count: v }),
+        error: errors.count,
+        input: {
+          //check instantly after blur
+          onBlur: () => form.checkValue('count'),
+          type: 'number',
+        },
+      },
+      {
+        type: 'small-color',
+        label: 'Role Color',
+        description: 'Pick a color',
+        value: value.color,
+        onChange: (color) => update({ color }),
+      },
+      {
+        type: 'color',
+        label: 'Role Color',
+        description: 'Pick a color',
+        value: value.color,
+        onChange: (color) => update({ color }),
+      },
+      {
+        type: 'small-date',
+        label: 'Date',
+        description: 'Select a date',
+        value: value.date,
+        onChange: (date: Date) => update({ date }),
+      },
+      {
+        type: 'date',
+        label: 'Date',
+        description: 'Select a date',
+        value: value.date,
+        onChange: (date: Date) => update({ date }),
+      },
+      {
+        type: 'custom-form',
+        label: 'Select a role',
+        description: 'Select something',
+        component: <RolesSelect value={value.role} onChange={(role) => update({ role })} />,
+        memorize: [value.role],
+      },
+      {
+        type: 'custom-form',
+        label: 'Channels',
+        description: 'Select a Channel',
+        component: (
+          <ChannelSelect value={value.channel} onChange={(channel) => update({ channel })} />
+        ),
+        memorize: [value.channel],
+      },
+      {
+        type: 'file',
+        label: 'Your File',
+        helperText: 'Support Gif, Jpg, Png and Svg files',
+        value: value.file,
+        onChange: (file) => update({ file }),
+        accept: {
+          'image/png': ['.png'],
+          'image/jpg': ['.jpg'],
+          'image/svg': ['.svg'],
+          'image/gif': ['.gif'],
+        },
+        picker: {
+          maxFiles: 2,
+        },
+      },
+    ],
   });
-
-  const combined = { ...data, ...value };
-  const component = createForm(
-    {
-      defaultMemorize: ['value', 'error'],
-    },
-    {
-      label: 'Message',
-      description: 'Hello World!!!',
-      type: 'input',
-      value: combined.message,
-      onChange: (message) => update({ message }),
-      error: errors.message,
-    },
-    {
-      type: 'input',
-      label: 'Count',
-      placeholder: 'Put a number',
-      value: combined.count ?? '0',
-      onChange: (v) => update({ count: v }),
-      error: errors.count,
-      input: {
-        //check instantly after blur
-        onBlur: () => form.checkValue('count'),
-        type: 'number',
-      },
-    },
-    {
-      type: 'small-color',
-      label: 'Role Color',
-      description: 'Pick a color',
-      value: combined.color,
-      onChange: (color) => update({ color }),
-    },
-    {
-      type: 'color',
-      label: 'Role Color',
-      description: 'Pick a color',
-      value: combined.color,
-      onChange: (color) => update({ color }),
-    },
-    {
-      type: 'small-date',
-      label: 'Date',
-      description: 'Select a date',
-      value: combined.date,
-      onChange: (date: Date) => update({ date }),
-    },
-    {
-      type: 'date',
-      label: 'Date',
-      description: 'Select a date',
-      value: combined.date,
-      onChange: (date: Date) => update({ date }),
-    },
-    {
-      type: 'custom-form',
-      label: 'Select a role',
-      description: 'Select something',
-      component: <RolesSelect value={combined.role} onChange={(role) => update({ role })} />,
-      memorize: [combined.role],
-    },
-    {
-      type: 'custom-form',
-      label: 'Channels',
-      description: 'Select a Channel',
-      component: (
-        <ChannelSelect value={combined.channel} onChange={(channel) => update({ channel })} />
-      ),
-      memorize: [combined.channel],
-    },
-    {
-      type: 'file',
-      label: 'Your File',
-      helperText: 'Support Gif, Jpg, Png and Svg files',
-      value: combined.file,
-      onChange: (file) => update({ file }),
-      accept: {
-        'image/png': ['.png'],
-        'image/jpg': ['.jpg'],
-        'image/svg': ['.svg'],
-        'image/gif': ['.gif'],
-      },
-      picker: {
-        maxFiles: 2,
-      },
-    }
-  );
-
-  return form.render(
-    <SimpleGrid columns={{ base: 1, md: 2 }} gap={3}>
-      {component}
-    </SimpleGrid>
-  );
 }
