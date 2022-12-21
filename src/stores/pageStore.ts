43ba5ca927bddf32b2498e6e21caf1c658c9fb48
SonMooSans <god63820869@gmail.com>
2022-12-21 19:31:45 +0800

    Create persist stores

--- src/stores/pageStore.ts
+++ src/stores/pageStore.ts
@@ -1,15 +1,37 @@
+import { Languages, translations } from 'config/translations';
 import create from 'zustand';
+import { persist } from 'zustand/middleware';
 
 export type PageStore = {
   sidebarIsOpen: boolean;
   setSidebarIsOpen: (v: boolean) => void;
+};
+
+export type PersistStore = {
   devMode: boolean;
   setDevMode: (v: boolean) => void;
+  lang: Languages;
+  setLang: (v: Languages) => void;
 };
 
 export const usePageStore = create<PageStore>((set) => ({
   sidebarIsOpen: false,
-  devMode: false,
-  setDevMode: (v) => set({ devMode: v }),
   setSidebarIsOpen: (v) => set({ sidebarIsOpen: v }),
 }));
+
+/**
+ * persist settings
+ */
+export const useSettingsStore = create(
+  persist<PersistStore>(
+    (set) => ({
+      devMode: false,
+      setDevMode: (v) => set({ devMode: v }),
+      lang: translations.defaultLang,
+      setLang: (v) => set({ lang: v }),
+    }),
+    {
+      name: 'settings',
+    }
+  )
+);
