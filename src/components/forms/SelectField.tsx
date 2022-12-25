31d5633833297f8ed8e7620b5dae144a954e0b59
SonMooSans <god63820869@gmail.com>
2022-12-25 21:22:05 +0800

    Clean code

--- src/components/forms/SelectField.tsx
+++ src/components/forms/SelectField.tsx
@@ -98,20 +98,3 @@ export const SelectField: SelectComponent = (props) => {
     />
   );
 };
-
-export function useSelectOptions<R, T extends Option>(data: R[] | null, mapper: (v: R) => T) {
-  return useMemo(() => {
-    const options = new Map<string, Option>();
-
-    data?.forEach((item) => {
-      const mapped = mapper(item);
-
-      options.set(mapped.value, mapped);
-    });
-
-    return {
-      options,
-      values: [...options.values()],
-    };
-  }, [data]);
-}
